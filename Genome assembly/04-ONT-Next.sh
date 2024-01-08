5e0456814199cfadaec10b6cc4b3ba68ea568d86
PengjuZ <109416356+PengjuZ@users.noreply.github.com>
2024-01-08 17:39:03 +0800

    Update 04-ONT-Next.sh

--- Genome assembly/04-ONT-Next.sh
+++ Genome assembly/04-ONT-Next.sh
@@ -3,5 +3,5 @@
 source /share/apps/anaconda3/bin/activate nextDenovo
 realpath $Data/ONT.gz > $OUT/run.fofn
 cp ./00-Software/NextDenovo/doc/run.cfg $OUT/run.cfg
-/share/home/zju_zhaopj/00-Software/NextDenovo/nextDenovo $OUT/run.cfg
+./NextDenovo/nextDenovo $OUT/run.cfg
 
